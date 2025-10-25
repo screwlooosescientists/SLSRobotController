9862edfdff4f6745f375d432cc4c69336ba417f8
pepijnftc <vanhappenpepijn@gmail.com>
2025-10-25 14:55:00 +0200

    atonomous blauw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Solutions/AutonomousTestblauw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Solutions/AutonomousTestblauw.java
@@ -34,102 +34,23 @@ public class AutonomousTestblauw extends LinearOpMode {
         // Wacht op start
         waitForStart();
 
-        // Draai naar links
-        turnLeft(0.5, 1030);
+
 
         // Rijdt nog een beetje vooruit
-        package org.firstinspires.ftc.teamcode.Solutions;
+        driveForward(0.6, 2250);
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        // Draai naar rechts
+        turnleft (0.7, 525);
 
-
-        @Autonomous(name="Simple Autonomous", group="Autonomous")
-        public class AutonomousTestblauw extends LinearOpMode {
-
-            private DcMotor left_front = null;
-            private DcMotor right_front = null;
-            private DcMotor right_back = null;
-            private DcMotor left_back = null;
-
-            @Override
-            public void runOpMode() {
-                // Hardware mapping
-                left_front = hardwareMap.get(DcMotor.class, "left_front");
-                right_front = hardwareMap.get(DcMotor.class, "right_front");
-                left_back = hardwareMap.get(DcMotor.class, "left_back");
-                right_back = hardwareMap.get(DcMotor.class,"right_back");
-
-                // Zet de richting goed (afhankelijk van hoe motoren gemonteerd zijn)
-                left_front.setDirection(DcMotor.Direction.FORWARD);
-                right_front.setDirection(DcMotor.Direction.REVERSE);
-                left_back.setDirection(DcMotorSimple.Direction.REVERSE);
-                right_back.setDirection(DcMotorSimple.Direction.FORWARD);
-
-                telemetry.addData("Status", "Gereed om te starten");
-                telemetry.update();
-
-                // Wacht op start
-                waitForStart();
-
-                driveForward(0.5, 2000);
-
-                // Draai naar links
-                turnLeft(0.5, 1030);
-
-                // Rijdt nog een beetje vooruit
-                driveForward(0.5, 2000);
-
-                telemetry.addData("Status", "Autonomous klaar");
-                telemetry.update();
-            }
-
-            private void turnLeft(double power, int timeMs) {
-                left_front.setPower(power);
-                right_front.setPower(-power);
-                left_back.setPower(power);
-                right_back.setPower(-power);
-                sleep(timeMs);
-                stopDriving();
-            }
-
-            // --- Hulpmethoden ---
-            private void driveForward(double power, int timeMs) {
-                left_front.setPower(power);
-                right_front.setPower(power);
-                left_back.setPower(power);
-                right_back.setPower(power);
-                telemetry.addData("status", "drive forward");
-                telemetry.update();
-                sleep(timeMs);
-                stopDriving();
-            }
-
-            private void turnRight(double power, int timeMs) {
-                left_front.setPower(power);
-                right_front.setPower(-power);
-                left_back.setPower(power);
-                right_back.setPower(-power);
-                sleep(timeMs);
-                stopDriving();
-            }
-
-            private void stopDriving() {
-                left_front.setPower(0);
-                right_front.setPower(0);
-                left_back.setPower(0);
-                right_back.setPower(0);
-            }
-        }
-
+        //rijdt naar rechts
+        driveobliquely(0.7, 100);
 
         telemetry.addData("Status", "Autonomous klaar");
         telemetry.update();
     }
 
-    private void turnLeft(double power, int timeMs) {
+    // --- Hulpmethoden ---
+    private void turnleft(double power, int timeMs) {
         left_front.setPower(power);
         right_front.setPower(-power);
         left_back.setPower(power);
@@ -138,26 +59,25 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         stopDriving();
     }
 
-    // --- Hulpmethoden ---
     private void driveForward(double power, int timeMs) {
         left_front.setPower(power);
-        right_front.setPower(power);
-        left_back.setPower(power);
+        right_front.setPower(-power);
+        left_back.setPower(-power);
         right_back.setPower(power);
         telemetry.addData("status", "drive forward");
         telemetry.update();
         sleep(timeMs);
         stopDriving();
     }
-
-    private void turnRight(double power, int timeMs) {
+private void driveobliquely(double power, int timeMs) {
         left_front.setPower(power);
-        right_front.setPower(-power);
+        right_front.setPower(power);
         left_back.setPower(power);
-        right_back.setPower(-power);
+        right_back.setPower(power);
         sleep(timeMs);
         stopDriving();
-    }
+}
+
 
     private void stopDriving() {
         left_front.setPower(0);
